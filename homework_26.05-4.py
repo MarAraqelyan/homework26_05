40b46e38fd0b0b92702dfe5ce512aa8d14690200
Mariam <mariam.araqelyan2004@gmail.com>
2023-06-01 15:47:37 +0400

    leap years

--- homework_26.05-4.py
+++ homework_26.05-4.py
@@ -1,6 +1,24 @@
-year = int(input("Enter a year:"))
+while True:
+	year = int(input("Enter a year:"))
+	if year != 1600:
+ 		break
 year_count=0
-for x in range(1600,year):
-    if x % 4==0 or (x % 100==0 and x%400==0):
-        year_count = year_count + 1
+if year > 1600:
+	for x in range(1600,year):
+		if x % 4 == 0:
+			if x % 100 == 0:
+				if x % 400 == 0:
+					year_count += 1
+			else:
+				year_count += 1
+if year < 1600:
+	for x in range(year,1600):
+		if x % 4 == 0:
+			if x % 100 == 0:
+				if x % 400 == 0:
+					year_count += 1
+
+			else:
+				year_count += 1
+
 print(year_count)
