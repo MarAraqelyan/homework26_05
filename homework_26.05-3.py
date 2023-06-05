c3dc331d32d125422e4196fa54e7fe18c0180d59
Mariam <mariam.araqelyan2004@gmail.com>
2023-06-05 12:21:14 +0400

    Entering two numbers

--- homework_26.05-3.py
+++ homework_26.05-3.py
@@ -1,4 +1,7 @@
-print("Enter two numbers")
-x=float(input("x="))
-y=float(input("y="))
+while True:
+	print("Enter two numbers")
+	x=float(input("x="))
+	y=float(input("y="))
+	if not x + y == 0:
+		break
 print(abs(x-y)/(x+y))
